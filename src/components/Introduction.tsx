dc9e60b609dcb244347032e0ec1a0054fb066031
Morgan James <mj522@exeter.ac.uk>
2024-08-21 22:29:26 +0100

    adding gradgig link to cv site

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./Introduction.css";
-const Introduction = () => {
-  return (
-    <div className="introduction">
-      <h1>Work In progress! </h1>
-      <p>
 
-      </p>
-    </div>
-  );
+const Introduction = () => {
+    return (
+        <div className="introduction">
+            <h1>Work In Progress!</h1>
+            <p>
+                Have a peek at what im working on : {" "}
+                <a href="https://gradgig.co.uk" target="_blank" rel="noopener noreferrer">
+                    gradgig.co.uk
+                </a>
+                .
+            </p>
+        </div>
+    );
 };
 
 export default Introduction;
